73f492a6b4432e3895867a475dfb1c8c3decae10
brudyr <ron@brudy.dev>
2023-11-12 22:37:07 +0100

    update neo-tree keybinds

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,8 +1,63 @@
+local find_in_finder = function(path)
+  local stat = vim.loop.fs_stat(path)
+  if not stat then
+    return
+  end
+
+  local cmd
+  if vim.fn.has("unix") == 1 and vim.fn.executable("xdg-open") == 1 then
+    cmd = { "xdg-open" }
+  elseif (vim.fn.has("mac") == 1 or vim.fn.has("unix") == 1) and vim.fn.executable("open") == 1 then
+    cmd = { "open" }
+    if stat.type ~= "directory" then
+      table.insert(cmd, "-R")
+    end
+  end
+
+  if not cmd then
+    return
+  end
+
+  vim.fn.jobstart(vim.fn.extend(cmd, { path or vim.fn.expand("<cfile>") }), { detach = true })
+end
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       close_if_last_window = true,
+      commands = {
+        system_open = function(state)
+          find_in_finder(state.tree:get_node():get_id())
+        end,
+        parent_or_close = function(state)
+          local node = state.tree:get_node()
+          if (node.type == "directory" or node:has_children()) and node:is_expanded() then
+            state.commands.toggle_node(state)
+          else
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end
+        end,
+        child_or_open = function(state)
+          local node = state.tree:get_node()
+          if node.type == "directory" or node:has_children() then
+            if not node:is_expanded() then -- if unexpanded, expand
+              state.commands.toggle_node(state)
+            else -- if expanded and has children, seleect the next child
+              require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+            end
+          else -- if not a directory just open it
+            state.commands.open(state)
+          end
+        end,
+      },
+      window = {
+        mappings = {
+          o = "system_open",
+          h = "parent_or_close",
+          l = "child_or_open",
+        },
+      },
     },
   },
 }

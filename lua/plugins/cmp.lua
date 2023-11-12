3b4f7100ad1a60a30984a032ad7cc88b3a8cb15e
brudyr <ron@brudy.dev>
2023-11-12 21:46:55 +0100

    allow j/k movement in cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,13 @@
+local cmp = require("cmp")
+
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      }),
+    },
+  },
+}

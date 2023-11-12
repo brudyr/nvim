3dbf95c2922e238a3f09f29967bbde39ae3bfce4
brudyr <ron@brudy.dev>
2023-11-12 22:50:05 +0100

    ensure defaults for mason and treesitter

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "pyright",
+      },
+    },
+  },
+}

4c63117709b5f57d88f43e019f6285b7e441c9a6
brudyr <ron@brudy.dev>
2024-01-11 21:38:42 +0100

    add toggle-bool

---
+++ lua/plugins/toogle-bool.lua
@@ -0,0 +1,20 @@
+return {
+  "gerazov/toggle-bool.nvim",
+  opts = {
+    mapping = "<leader>i",
+    additional_toggles = {
+      Yes = "No",
+      On = "Off",
+      ["0"] = "1",
+      Enable = "Disable",
+      Enabled = "Disabled",
+      First = "Last",
+      Before = "After",
+      Persistent = "Ephemeral",
+      Internal = "External",
+      Ingress = "Egress",
+      Allow = "Deny",
+      All = "None",
+    },
+  },
+}
